18063514ad06645ac632e63785e73612191938ae
George <sdi1600094@di.uoa.gr>
2019-01-08 21:28:44 +0200

    First commit

---
+++ cmake-build-debug/CMakeFiles/Ergasia_2018_2019.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ergasia_2018_2019.dir/main.c.o"
+  "Ergasia_2018_2019.pdb"
+  "Ergasia_2018_2019"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Ergasia_2018_2019.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Ergasia_2018_2019.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Ergasia_2018_2019.
+# This may be replaced when dependencies are built.
