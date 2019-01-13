a3d7fe636cdf86f62609069eda4446a71cfc3c05
George <sdi1600094@di.uoa.gr>
2019-01-13 06:59:26 +0200

    minor change

--- Include/macros.h
+++ Include/macros.h
@@ -2,8 +2,6 @@
 #define DB_EX1_MACROS_H
 
 #define __MALLOC(size, type) ((type*) malloc((size) * sizeof(type)))
-#define __MALLOC_BYTES(bytes) ((void*) malloc((bytes)))
-#define BYTE_POINTER(object) ((void*) object)
 
 #define STR_COPY(dest, source, len) \
 { \
